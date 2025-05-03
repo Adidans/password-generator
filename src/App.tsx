fee7a0f0121fc6216a39cb54d8ee006285d4391c
Adidans <adrians.uvis@gmail.com>
2025-05-03 11:48:07 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+    return (
+        <div>
+            <h1>Password Generator</h1>
+            <Button>Generate</Button>
+        </div>
+    );
+}
+
+export default App;
