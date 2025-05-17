db7bdba7a5f3699ee1c70366f97ab6db8ed925a4
Adidans <adrians.uvis@gmail.com>
2025-05-17 22:12:35 +0300

    Add ArrowIcon component and integrate it into the App button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
+import ArrowIcon from "@/components/icon-arrow";
 
 function App() {
     return (
-        <div>
-            <h1>Password Generator</h1>
-            <Button>Generate</Button>
+        <div className="h-screen flex flex-col justify-center items-center bg-linear-to-r from-gray-900 to-gray-950 font-jetbrains-mono">
+            <h1 className="text-gray-600 text-sm">Password Generator</h1>
+            <Button className="bg-green-200 text-gray-800 rounded-none px-26 py-4 text-sm uppercase flex items-center justify-center">
+                <div className="flex items-center gap-4">
+                    <p>Generate</p>
+                    <ArrowIcon />
+                </div>
+            </Button>
         </div>
     );
 }
